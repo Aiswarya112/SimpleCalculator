2e1ffeae4d45b7cdb8292ee14040dd52c8910d44
Aiswarya <sweetpainting90977cups@gmail.com>
2023-07-30 18:13:37 +0530

    Initial commit

---
+++ mycalc.js
@@ -0,0 +1,14 @@
+function ac()
+{
+    document.getElementById("result").value = "";
+}
+
+function show(input)
+{
+    document.getElementById("result").value+=input;
+}
+function calc()
+{
+    var output = eval(document.getElementById("result").value);
+    document.getElementById("result").value = output;
+}
---
+++ trial.js
@@ -0,0 +1,9 @@
+let mark = prompt("Enter your marks")
+if (mark>=50)
+{
+    console.log("Passed")
+}
+else
+{
+    console.log("Failed")
+}
